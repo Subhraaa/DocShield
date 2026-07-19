65b30380617eaecda326800f8e7e463931aa60e3
Subhra <b220059@iiit-bh.ac.in>
2026-07-19 10:48:36 +0530

    Initial commit: local project setup

---
+++ docshield_backend/src/test/java/com/subhra/docshield/DocshieldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subhra.docshield;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocshieldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
